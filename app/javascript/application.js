c21c17c736891f726155edded19e873803494d3e
Pramod <pramod@digitalthrottle.com>
2023-03-17 21:28:10 +0530

    datatable integration

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,7 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import DataTable from 'datatables.net-bs5';
+
+window.DataTable = DataTable
 document.addEventListener("turbo:load", () => {
     const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
     const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+    ele = document.getElementById('example')
+    let table = new DataTable('#example');
   })
+
+  document.addEventListener('DOMContentLoaded', function () {
+ 
+    });
